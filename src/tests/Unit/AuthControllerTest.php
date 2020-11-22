d741ce4b219c3cfb48ce191ca27790fdfcbcb38e
Alireza <alirezamaleki944@gmail.com>
2020-11-22 11:31:19 +0330

    Auth Test for register

---
+++ src/tests/Unit/AuthControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\V01\Auth;
+
+use PHPUnit\Framework\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+
+    public function testRegister()
+    {
+        $this->assertTrue(1>0);
+    }
+}
